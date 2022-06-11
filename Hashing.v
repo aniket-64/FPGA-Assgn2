d82b879132247490c3fb23b0c6792c5ce38da7f8
Harsh-Murdeshwar <106463694+Harsh-Murdeshwar@users.noreply.github.com>
2022-06-11 21:09:23 +0530

    Add files via upload

--- Hashing.v
+++ Hashing.v
@@ -3,41 +3,36 @@
 
 module index_calc (input [31:0] num , output [4:0] i1 , output [4:0] i2);
 
-assign i1= (num*num*num)%20;
+assign i1= ((num%20)*(num%20)*(num%20))%20;
 assign i2= ((2**num)+num)%20;
 endmodule
 
-module hashing (val );
-
-input wire [31:0] val;
-
-reg clk;
+module hashing (input [31:0] val);
 
 reg [31:0] table1 [19:0];
 reg table1_filled [19:0];
 reg [31:0] table2 [19:0];
 reg table2_filled [19:0];
 
-reg [31:0] num,temp;
+reg [31:0] num;
+reg [31:0] temp;
 wire [4:0] i1,i2;
 
-reg status , completed , flag;
+reg status , completed , clk;
 
 index_calc index (num , i1 ,i2);
 
+
 initial begin
-
-    status<=0;
-    completed<=0;
-    clk<=0;
+     clk<=0;  
+    
     for (integer i=0 ; i<20 ; i=i+1) begin
-    table1_filled [i] = 0 ;
-    table2_filled [i] = 0 ;
-    table1 [i] = 0 ;
-    table2 [i] = 0 ;
-end;
+    table1_filled [i] <= 0 ;
+    table2_filled [i] <= 0 ;
+    table1 [i] <= 0 ;
+    table2 [i] <= 0 ;
+    end;
 
-#1;
 table1 [4] <= 14 ;  table1_filled [4] <=1;
 table1 [9] <= 89;  table1_filled [9] <=1;
 table1 [12] <=48 ;  table1_filled [12]<=1;
@@ -56,43 +51,51 @@ table2 [5] <=13;  table2_filled [5] <=1;
     // end;    
 end
 
-always #2 clk=~clk;
+always #20 clk=~clk;
+always @(val) begin
+    force num=val;
+    status<=0;
+    completed<=0;
+end
 
-always @(status) begin
-    #20
-    $display("always block entered");
-    if (completed == 1) begin
-         $finish;
-    end
-    else if (status == 0 ) begin
-        $display("if block entered");
-        flag = table1_filled[i1];
-        $display("%d",i1);
-        if (flag==0) begin
-            $display("if block entered");
-            table1[i1]=num;
-            table1_filled[i1]=1'b1;
-            completed = 1;
+always @(status or negedge completed) begin
+    
+    // $display("always block entered");
+
+
+    if (status == 0 ) begin
+        // $display("if block entered");
+        
+        
+        if (table1_filled[i1]==1) begin
+            // $display("if block entered");
+            release num;
+            table1[i1]<=num;
+            num<=table1[i1];
+            status <= ~ status;            
         end
+
         else begin
-            temp=table1[i1];
-            table1[i1]=num;
-            num=temp;
-            status=~status;
+            //temp=table1[i1];
+            // $display("%b",i1);
+            // $display("this block");
+            table1[i1]<=num;
+            table1_filled[i1]<=1'b1;
+            completed <= 1;
         end
     end
 
     else if (status == 1) begin
-        if (table2_filled[i2]==0) begin
-            table2[i2]=num;
-            table2_filled[i1]=1;
-            completed = 1;
+        if (table2_filled[i2]==1) begin
+            release num;
+            table2[i2]<=num;
+            num<=table2[i2];
+            status=~status;            
         end
         else begin
-            temp=table2[i2];
-            table2[i2]=num;
-            num=temp;
-            status=~status;
+            table2[i2]<=num;
+            table2_filled[i2]<=1;
+            completed <= 1; 
         end
     end
 end   
--- hashing_tb.v
+++ hashing_tb.v
@@ -7,16 +7,55 @@ reg [31:0] val;
 hashing test (val);
 
 initial begin
-    val=70;
-    // test.table1_filled[in]=1;
+    $dumpfile("check.vcd");
+    $dumpvars(0,test.hashing);
 
-    #10
-    for (integer i=0 ; i<20 ;i++) begin
-        $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
-        , i , test.table2_filled[i] , test.table2[i]);
-    end;
+    #20     val=50;          
+    #20    val=61;          
+    #20    val=44;     
+    #20    val=14;        
+    #20   val=82;  
+        
+
 end
 
+initial begin
+    #30
+        $display("\n%d added",val);
+        for (integer i=0 ; i<20 ;i++) begin
+            $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
+            , i , test.table2_filled[i] , test.table2[i]);
+        end 
+
+    #20
+        $display("\n%d added",val);
+        for (integer i=0 ; i<20 ;i++) begin
+            $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
+            , i , test.table2_filled[i] , test.table2[i]);
+        end 
+
+    #20
+        $display("\n%d added",val);
+        for (integer i=0 ; i<20 ;i++) begin
+            $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
+            , i , test.table2_filled[i] , test.table2[i]);
+        end 
+
+    #20
+        $display("\n%d added",val);
+        for (integer i=0 ; i<20 ;i++) begin
+            $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
+            , i , test.table2_filled[i] , test.table2[i]);
+        end 
+
+    #20
+        $display("\n%d added",val);
+        for (integer i=0 ; i<20 ;i++) begin
+            $display("%d : %d %d   %d : %d %d", i , test.table1_filled[i] , test.table1[i]
+            , i , test.table2_filled[i] , test.table2[i]);
+        end   
+    $finish;
+end
 endmodule
 
 
@@ -29,10 +68,13 @@ endmodule
 // index_calc test (num , i1 , i2);
 
 // initial begin
+//     $dumpfile("check.vcd");
+//     $dumpvars(0,index_tb);
 //     num=87;
+//     $display("%d: i1=%d i2=%d",num,i1,i2);
 // end
 
 // initial begin
-//     $monitor("%d: i1=%d i2=%d",num,i1,i2);
+    
 // end
 // endmodule
